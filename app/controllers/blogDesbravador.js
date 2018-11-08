746adf525fc48ea7436689658e7bd4cf662936e3
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-11-08 19:13:34 -0200

    subindo

--- app/controllers/blogDesbravador.js
+++ app/controllers/blogDesbravador.js
@@ -2,4 +2,6 @@
 
 module.exports.cadastrarBlog = (app, req, res) => {
     let form_blog = req.body;
+
+    
 }
--- app/routes/router_blog.js
+++ app/routes/router_blog.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
     app.get('/blog/inserir/conteudo', (req, res) => {
-
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        //app.app
     });
 }
